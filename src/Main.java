d911c080568d3cd012072801388d25d9b464c789
Fe1ixcc <yy20240024@gmail.com>
2024-10-24 17:21:56 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        MenuItem[] MenuItem;
+        MenuItem = new MenuItem[3];
+        MenuItem[0]=new MenuItem("Vegetable salad","Abundant vegetables and salad dressing.",12);
+        MenuItem[1]=new MenuItem("Super Chicken","Grilled chicken breast topped with aromatic spices and vegetable.",15);
+        MenuItem[2]=new MenuItem("BBQ Bacon Hamburger","A juicy beef pie in black pepper sauce and topped with crispy bacon bits.",13);
+        for(int i = 0; i < MenuItem.length ; i++){
+            System.out.print("Welcome to order food");
+            System.out.println(i+1+". "+MenuItem[i].getName());
+            System.out.println("Price is" + MenuItem[i].getPrice());
+        }
+
+       System.out.println("Please enter the order item number:");
+    Scanner scanner = new Scanner(System.in);
+    float menuId = scanner.nextFloat();
+
+        System.out.println("Your choice is " + MenuItem[(int)menuId - 1].getName() );
+        if (menuId == 1) {
+            System.out.println("The price of Vegetable salad is 12");
+            System.out.println("Please enter the number of Vegetable salad:");
+            int num1 = scanner.nextInt();
+            System.out.println("The total price is " + num1 * 12);
+        }else if (menuId == 2) {
+            System.out.println("The price of Super Chicken is 15");
+            System.out.println("Please enter the number of Super Chicken:");
+            int num2 = scanner.nextInt();
+            System.out.println("The total price is " + num2 * 15);
+        }else if (menuId == 3) {
+            System.out.println("The price of BBQ Bacon Hamburger is 13");
+            System.out.println("Please enter the number of BBQ Bacon Hamburger:");
+            int num3 = scanner.nextInt();
+            System.out.println("The total price is " + num3 * 13);
+        }else
+            System.out.println("Invalid order item number");
+
+    }
+
+}
+
+
+
+
+
+
---
+++ src/MenuItem.java
@@ -0,0 +1,41 @@
+public class MenuItem {
+    public String name;
+    public String description;
+    public int price;
+    public static MenuItem[] menu = {
+            new MenuItem("Vegetable salad", "Abundant vegetables and salad dressing.", 12),
+            new MenuItem("Super Chicken", "Grilled chicken breast topped with aromatic spices and vegetable.", 15),
+            new MenuItem("BBQ Bacon Hamburger", "A juicy beef pie in black pepper sauce and topped with crispy bacon bits.", 13)
+     };
+
+    public MenuItem(String name, String description, int price){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
