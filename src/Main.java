75fbf3480adf3479ebbf46219b51fa5bf6ed21cb
Fe1ixcc <yy20240024@gmail.com>
2024-10-24 20:38:59 +0800

    4th
    4

--- src/Main.java
+++ src/Main.java
@@ -16,28 +16,34 @@ public class Main {
         System.out.println("Please enter the order item number:");
         Scanner scanner = new Scanner(System.in);
         float menuId = scanner.nextFloat();
-        if (menuId <= 3) {
-            System.out.println("Your choice is " + MenuItem[(int) menuId - 1].getName());
-            if (menuId == 1) {
-                System.out.println("The price of Vegetable salad is 12");
-                System.out.println("Please enter the number of Vegetable salad:");
-                int num1 = scanner.nextInt();
-                System.out.println("The total price is " + num1 * 12);
-            } else if (menuId == 2) {
-                System.out.println("The price of Super Chicken is 15");
-                System.out.println("Please enter the number of Super Chicken:");
-                int num2 = scanner.nextInt();
-                System.out.println("The total price is " + num2 * 15);
-            } else if (menuId == 3) {
-                System.out.println("The price of BBQ Bacon Hamburger is 13");
-                System.out.println("Please enter the number of BBQ Bacon Hamburger:");
-                int num3 = scanner.nextInt();
-                System.out.println("The total price is " + num3 * 13);
+        if (menuId > 3) {
+            int a = 1;
+            while (a < 2) {
+                System.out.println("Please input the correct number");
+                menuId = scanner.nextFloat();
+                if (menuId <= 3) {
+                    a++;
+                    System.out.println("Your choice is " + MenuItem[(int) menuId - 1].getName());
+                    if (menuId == 1) {
+                        System.out.println("The price of Vegetable salad is 12");
+                        System.out.println("Please enter the number of Vegetable salad:");
+                        int num1 = scanner.nextInt();
+                        System.out.println("The total price is " + num1 * 12);
+                    } else if (menuId == 2) {
+                        System.out.println("The price of Super Chicken is 15");
+                        System.out.println("Please enter the number of Super Chicken:");
+                        int num2 = scanner.nextInt();
+                        System.out.println("The total price is " + num2 * 15);
+                    } else if (menuId == 3) {
+                        System.out.println("The price of BBQ Bacon Hamburger is 13");
+                        System.out.println("Please enter the number of BBQ Bacon Hamburger:");
+                        int num3 = scanner.nextInt();
+                        System.out.println("The total price is " + num3 * 13);
+                    }
+                }
             }
         }
     }
 }
-
-
 
 
