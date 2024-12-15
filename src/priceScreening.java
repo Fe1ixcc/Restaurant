fe81cfd634add27c40917a8cbff88cfdaf36f122
LingGan659 <2269606612@qq.com>
2024-12-15 23:09:23 +0800

    Create priceScreening.java
    13th

---
+++ src/priceScreening.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class priceScreening {
+    public void Print() {
+        System.out.println(" What is your budget ?");
+        Scanner scanner = new Scanner(System.in);
+        float Budget = scanner.nextInt();
+        if (Budget<12){
+            System.out.println(" Sorry, your budget is too low. ");
+        }
+        if (Budget<13 && Budget>=12){
+            System.out.println(" We recommend Vegetable salad to you.");
+        }
+        if (Budget<15 && Budget>=13) {
+            System.out.println(" We recommend Vegetable salad and Super Chicken to you.");
+        }
+        if (Budget>=15){
+            System.out.println(" We recommend Vegetable salad , Super Chicken and BBQ Bacon Hamburger to you.");
+        }
+    }
+
+}
