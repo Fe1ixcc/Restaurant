dc0b12fab397733f43c5d654943e28db2330cddc
Fe1ixcc <yy20240024@gmail.com>
2024-12-16 20:30:23 +0800

    15th
    15th

--- src/basicMethodForStuff.java
+++ src/basicMethodForStuff.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class basicMethodForStuff {
-    public void stuffMethod(){
+    public void stuffMethod() {
         System.out.println("""
                 What do you want to do next?
                 1)List menu;
@@ -8,13 +10,36 @@ public class basicMethodForStuff {
                 4)
                 0)exit
                 """);
-    }
-    public void menuPrint() {
-        System.out.println("Welcome to order food");
-        for (int i = 0; i < MenuItem.menu.length; i++) {
-            System.out.println((i + 1) + ". " + MenuItem.menu[i].getName());
-            System.out.println(MenuItem.menu[i].getDescription());
-            System.out.println(MenuItem.menu[i].getPrice());
+        Scanner scanner = new Scanner(System.in);
+        int priceScreening = scanner.nextInt();
+
+        switch (priceScreening) {
+            case 1:
+                menuPrint();
+            case 2:
+                addDish();
+                break;
         }
     }
+        public void addDish () {
+        System.out.println("What do you want to add?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("What is the description?");
+        String description = scanner.nextLine();
+        System.out.println("What is the price?");
+        int price = scanner.nextInt();
+        MenuItem.menu[MenuItem.menu.length] = new MenuItem(name, description, price);
+        System.out.println("Added successfully");
+        }
+
+        public void menuPrint () {
+            System.out.println("Welcome to order food");
+            for (int i = 0; i < MenuItem.menu.length; i++) {
+                System.out.println((i + 1) + ". " + MenuItem.menu[i].getName());
+                System.out.println(MenuItem.menu[i].getDescription());
+                System.out.println(MenuItem.menu[i].getPrice());
+            }
+        }
+
 }
