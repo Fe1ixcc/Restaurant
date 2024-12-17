447371c6733826eea3993589badf14ea57146c64
JiaqiJin114 <2029946428@qq.com>
2024-12-17 16:52:31 +0800

    19th
    added Junit.

--- src/basicMethodTest.java
+++ src/basicMethodTest.java
@@ -1,16 +1,42 @@
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 class basicMethodTest {
+    private InputStream originalIn;
+    private basicMethod service;
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(originalOut);
+        outContent.reset();
+    }
 
     @Test
-    public void menuPrintTest() {
+    public void testMenuPrint() {
         basicMethod.menuPrint();
+
+        Assertions Assertions = null;
+
+        Assertions.assertTrue(outContent.toString().contains("1. " + MenuItem.menu[0].getName()));
+        Assertions.assertTrue(outContent.toString().contains(MenuItem.menu[0].getDescription()));
+        Assertions.assertTrue(outContent.toString().contains(MenuItem.menu[0].getPrice() + "$"));
     }
 
     @Test
     public void interactiveSystemTest() {
         basicMethod.interactiveSystem();
-
     }
 }
--- src/priceScreening.java
+++ src/priceScreening.java
@@ -4,16 +4,16 @@ public class priceScreening {
         System.out.println("What is your budget ?");
         Scanner scanner = new Scanner(System.in);
         float Budget = scanner.nextInt();
-        if (Budget<MenuPrice.menu[0].price){
+        if (Budget< MenuItem.menu[0].price){
             System.out.println("Sorry, your budget is too low. ");
         }
-        if (Budget<MenuPrice.menu[2].price && Budget>=MenuPrice.menu[0].price){
+        if (Budget< MenuItem.menu[2].price && Budget>= MenuItem.menu[0].price){
             System.out.println("We recommend Vegetable salad to you.");
         }
-        if (Budget<MenuPrice.menu[1].price && Budget>=MenuPrice.menu[2].price) {
+        if (Budget< MenuItem.menu[1].price && Budget>= MenuItem.menu[2].price) {
             System.out.println("We recommend Vegetable salad and Super Chicken to you.");
         }
-        if (Budget>=MenuPrice.menu[1].price){
+        if (Budget>= MenuItem.menu[1].price){
             System.out.println("We recommend Vegetable salad , Super Chicken and BBQ Bacon Hamburger to you.");
         }
     }
